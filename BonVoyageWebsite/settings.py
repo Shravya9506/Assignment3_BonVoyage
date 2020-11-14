ea27561624d5e1aac912c67b566cebe760dc91ed
ShravyaChandiri <schandiri@unomaha.edu>
2020-11-14 13:38:43 -0600

    Adding postgres settings

--- BonVoyageWebsite/settings.py
+++ BonVoyageWebsite/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'BonVoyageWebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BonVoyageWebsite',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'PORT': '5433'
     }
 }
 
